1dac13b7d15b064e6ed1e803aaa8ca608c19dbba
GIANG <nguyengiangbl2k@gmail.com>
2023-06-06 15:46:06 +0900

    sua lan 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <title>Laravel</title>
+        <title>Laravel</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
